1d75eda2ef80435c2ed7950d06e0119cf701de55
maychine <fatimamaychine@gmail.com>
2020-05-30 07:04:15 +0100

    use html instead of string value

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -16,7 +16,7 @@ class Counter extends Component {
 
     formatCount() {
         const { count } = this.state;
-        return count === 0 ? 'zero' : count;
+        return count === 0 ? <span>Zero</span> : count;
     }
 }
 
