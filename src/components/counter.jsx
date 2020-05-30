0aaa345dc7a8f06816de29d3f452db53bca39fa1
maychine <fatimamaychine@gmail.com>
2020-05-30 07:07:04 +0100

    set dynamic attribute in an element

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 class Counter extends Component {
 
     state = {
-        count: 0
+        count: 0,
+        imageUrl: 'https://picsum.photos/200'
     };
 
     render() {
         return (
             <React.Fragment>
+                <img src={this.state.imageUrl} alt="" />
                 <span>{this.formatCount()}</span>
                 <button>Increment</button>
             </React.Fragment>);
@@ -16,6 +18,7 @@ class Counter extends Component {
 
     formatCount() {
         const { count } = this.state;
+
         return count === 0 ? <span>Zero</span> : count;
     }
 }
