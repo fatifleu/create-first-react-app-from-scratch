23a64ea8f97cd6aa6f97deb975564950ab37386f
maychine <fatimamaychine@gmail.com>
2020-05-30 18:22:17 +0100

    conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,34 +5,26 @@ class Counter extends Component {
     //object to set all the data of this component
     state = {
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        tags: []
     };
 
+    renderTags() {
+        if (this.state.tags.length === 0) return 'no tags here !'
+        else return this.state.tags.map(tag => <li key={tag} >{tag}</li>)
+    }
 
 
     render() {
 
         return (
             <div>
-                <span className={this.getBadgeClasses()} >{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
                 <ul>
-                    {this.state.tags.map(tag => <li key={tag} >{tag}</li>)}
+                    {this.renderTags()}
                 </ul>
             </div>);
     }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
 
-    formatCount() {
-        const { count } = this.state;
-
-        return count === 0 ? <span>Zero</span> : count;
-    }
 }
 
 export default Counter;
